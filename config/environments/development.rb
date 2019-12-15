6916b3284a8860a5b89c77a9a622eae471e28d63
zaagan <ojthapa@gmail.com>
2019-12-15 11:33:55 -0800

    ruby-react-boggle.herokuapp.com Whitelisted

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "ruby-react-boggle.herokuapp.com"
 
 
   # In the development environment your application's code is reloaded on
